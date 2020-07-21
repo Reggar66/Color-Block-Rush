17d74b4b72d8b7517303fa86499cf4f73268dd73
Reggar66 <47181267+Reggar66@users.noreply.github.com>
2020-07-21 22:28:15 +0200

    Changed name for powerup correction script (from LifeCorrection)

--- Assets/Scripts/PowerUpPositionCorrection.cs
+++ Assets/Scripts/PowerUpPositionCorrection.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifePositionCorrection : MonoBehaviour
+public class PowerUpPositionCorrection : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
